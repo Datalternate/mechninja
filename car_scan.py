8d476c9841ad41d929788adac180730064b116e0
Datalternate <148906755+Datalternate@users.noreply.github.com>
2025-10-26 21:27:49 +0300

    Integrate OBD connection and live data retrieval

--- car_scan.py
+++ car_scan.py
@@ -1,1 +1,14 @@
-get_fault_codes(demo=True)
+import obd
+from utils.matcher import match_specs
+from utils.fault_codes import get_fault_codes
+
+connection = obd.OBD()
+commands = [obd.commands.RPM, obd.commands.SPEED, obd.commands.THROTTLE_POS]
+live_data = {}
+
+for cmd in commands:
+    response = connection.query(cmd)
+    live_data[cmd.name] = str(response.value)
+
+match_specs("sample_model", live_data)
+get_fault_codes(connection)
